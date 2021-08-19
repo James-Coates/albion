d217d652caa3d7aebb8b4fec17a9b21765994b19
james-coates <james.coates@me.com>
2021-08-19 19:52:07 +0200

    turn off cdn

--- sanity-client.config.js
+++ sanity-client.config.js
@@ -6,7 +6,8 @@ const options = {
   // Find your project ID and dataset in `sanity.json` in your studio project
   dataset: 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  useCdn: process.env.NODE_ENV === 'production',
+  // useCdn: process.env.NODE_ENV === 'production',
+  useCdn: false,
   apiVersion: '2021-04-17', // use a UTC date string
   // useCdn == true gives fast, cheap responses using a globally distributed cache.
   // Set this to false if your application require the freshest possible
