bd0339b35127a3eae64dda101e91f3b647775eba
james-coates <james.coates@me.com>
2021-09-16 15:15:50 +0200

    correct analytics script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,7 +16,7 @@ export default class Document extends NextDocument {
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
   `;
 
   static async getInitialProps(ctx: any) {
